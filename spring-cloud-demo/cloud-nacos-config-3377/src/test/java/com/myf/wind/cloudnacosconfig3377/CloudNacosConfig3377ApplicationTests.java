2841f291d2bfb07c7dfafeba12f024381ebc534a
windmyf <windmyf701@163.com>
2023-03-22 07:16:08 +0800

    nacos配置中心

---
+++ spring-cloud-demo/cloud-nacos-config-3377/src/test/java/com/myf/wind/cloudnacosconfig3377/CloudNacosConfig3377ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myf.wind.cloudnacosconfig3377;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudNacosConfig3377ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
